dc33bdd5c20a9db691298c06b8bf327f3f3216aa
zccz14 <zccz14@outlook.com>
2017-01-13 16:37:47 +0800

    rename and ...

--- test/ava/index.js
+++
@@ -1,5 +0,0 @@
-import test from 'ava';
-import request from 'supertest';
-import app from '../../server';
-
-test('aa', t => request(app).delete('/system').expect(200).then(res => res.body).then(console.log));
